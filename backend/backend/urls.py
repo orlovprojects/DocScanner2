e649d63dceb53888e91ede95959704e7da8625e9
Your Name <you@example.com>
2025-07-21 14:44:04 +0300

    fix:

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from docscanner_app import views
-
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail import urls as wagtail_urls
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('django-admin/', admin.site.urls),         # стандартный Django admin
+    path('admin/', include(wagtailadmin_urls)),     # Wagtail admin (теперь по /admin/)
     path('api/', include('docscanner_app.urls')),
 ]
 
